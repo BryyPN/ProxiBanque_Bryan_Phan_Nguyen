84fa2d6eea6d3a51a62fc894c8f600f11197bfc7
BryyPN <bryanphannguyenpro@gmail.com>
2025-11-20 11:50:22 +0100

    le reste

---
+++ ProxiBanque_Bryan_Phan_Nguyen/src/main/java/org/formation/proxibanque/controller/AccountController.java
@@ -0,0 +1,37 @@
+package org.formation.proxibanque.controller;
+
+import org.formation.proxibanque.entity.Account;
+import org.formation.proxibanque.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Account> getAccountById(@PathVariable Long id) {
+        return accountService.getAccountById(id);
+    }
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+    }
+}
