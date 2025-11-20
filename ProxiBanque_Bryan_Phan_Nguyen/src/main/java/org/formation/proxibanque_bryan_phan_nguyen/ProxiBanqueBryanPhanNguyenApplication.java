e62940bfa45cab4a617b8027630f5f44e6980bb8
Bryan Phan Nguyen <hweiyn@MacBook-Air-de-Bryan.local>
2025-11-20 10:36:09 +0100

    init

---
+++ ProxiBanque_Bryan_Phan_Nguyen/src/main/java/org/formation/proxibanque_bryan_phan_nguyen/ProxiBanqueBryanPhanNguyenApplication.java
@@ -0,0 +1,13 @@
+package org.formation.proxibanque_bryan_phan_nguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueBryanPhanNguyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanqueBryanPhanNguyenApplication.class, args);
+    }
+
+}
---
+++ ProxiBanque_Bryan_Phan_Nguyen/src/test/java/org/formation/proxibanque_bryan_phan_nguyen/ProxiBanqueBryanPhanNguyenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.proxibanque_bryan_phan_nguyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiBanqueBryanPhanNguyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
